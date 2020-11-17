295004e4217173b862ae72ac667fa3c5fc11f96c
Han Dongwon <hdw0903@naver.com>
2020-11-17 21:29:40 +0900

    papago router

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 import { Router } from 'express';
 import * as books from './api/books';
+import * as papago from './api/papago';
+
 const router = Router();
 
 router.get('/books', books.getBooks);
@@ -7,4 +9,6 @@ router.get('/books/:id', books.getBookById);
 router.post('/books', books.createBook);
 router.put('/books/:id', books.updateBookById);
 router.delete('/books/:id', books.deleteBookById);
+router.post('/papago/detectLangs', papago.detectLangs);
+router.post('/papago/n2mt', papago.n2mt);
 export default router;
