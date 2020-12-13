1aca2858fd2091564933c32b1e994556290855a8
Han Dongwon <hdw0903@naver.com>
2020-12-13 15:08:35 +0900

    nginx cors test

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ const corsOptions = {
   },
 };
 // app.use(cors(corsOptions));
-app.use(cors());
+// app.use(cors());
 app.use('/api', apiRouter);
 app.get('/', (req, res) => {
   res.send('hello world ');
