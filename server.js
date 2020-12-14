2786b0424bc1a7866901f5e57c08f52c1160bdac
Han Dongwon <hdw0903@naver.com>
2020-12-14 17:45:19 +0900

    cors-test

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.use(bodyParser.json());
 if (process.env.NODE_ENV === 'development') {
   app.use(cors());
 }
+app.use(cors());
 app.use('/api', apiRouter);
 app.get('/', (req, res) => {
   res.send('hello world ');
